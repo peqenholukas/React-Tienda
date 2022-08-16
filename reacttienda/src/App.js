4445246912ca060dd685d4cff95b3556030f69fc
peqenho <lucasotero1844@gmail.com>
2022-08-16 18:49:09 -0300

    TP 1

---
+++ reacttienda/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>¡Hola mundo!</h1>
+    <h3>Otro texto</h3>
+    </>
+  );
+}
+
+export default App;
