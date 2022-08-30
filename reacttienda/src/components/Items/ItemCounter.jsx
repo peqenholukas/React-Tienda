20256ffa677c8da86d4b6a95881d12649b5b917b
peqenho <lucasotero1844@gmail.com>
2022-08-30 17:52:23 -0300

    Puliendo detalles del TP

--- reacttienda/src/components/Items/ItemCounter.jsx
+++ reacttienda/src/components/Items/ItemCounter.jsx
@@ -4,35 +4,42 @@ import { useEffect } from 'react'
 import './Items.css';
 
 const ItemCounter = (props) => {
-    
-    const stock = 3
-    
+
+    const stock = 0
+
     const [counter, setCounter] = useState(0)
+
+    if (stock == 0) {
+
+    } else {
         
-    const incrementClick = () =>{
-        if(counter < stock){
+    }
+    const incrementClick = () => {
+        if (stock == 0) {
+            alert("No hay stock")
+        } else if(counter < stock){
             setCounter(counter + 1)
         }
-    } 
+    }
 
-    const decrementClick = () =>{
-        if(counter == 0){
+    const decrementClick = () => {
+        if (counter == 0) {
         } else {
             setCounter(counter - 1)
         }
     }
 
     return (
-    <div className='cont'>
-        <div className='cont-btns'>
-            <button onClick={decrementClick}>−</button>
-            <p>{counter}</p>
-            <button onClick={incrementClick}>+</button>
+        <div className='cont'>
+            <div className='cont-btns'>
+                <button onClick={decrementClick}>−</button>
+                <p>{counter}</p>
+                <button onClick={incrementClick}>+</button>
+            </div>
+            <button id='cartbtn'>Agregar al carrito</button>
         </div>
-        <button id='cartbtn'>Agregar al carrito</button>
-    </div>
-    
-  )
+
+    )
 }
- 
+
 export default ItemCounter
