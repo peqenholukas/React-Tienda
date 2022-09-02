70b907cb870bffbb51ec1dd23cf829ea46c26a80
peqenho <lucasotero1844@gmail.com>
2022-09-01 22:00:46 -0300

    Erorres

--- reacttienda/src/components/Items/Item.jsx
+++ reacttienda/src/components/Items/Item.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-export const Item = (title, img, price) => {
+export const Item = (items2) => {
+    console.log(items2.items2.price)
   return (
     <article>
-        <h3>{title}</h3>
-        <h3>{price}</h3>
-        <img src={img} alt="" />
+        <h3>{items2.items2.title}</h3>
+        <h3>{items2.items2.price}</h3>
+        <img src={items2.items2.img} alt="" />
     </article>
   )
 }
